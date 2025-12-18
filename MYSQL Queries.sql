8757a65645f6931f2f968f3a44a44085170a68c1
Nandan098 <149095610+Nandan098@users.noreply.github.com>
2025-12-19 01:18:15 +0530

    Update MYSQL Queries.sql

--- MYSQL Queries.sql
+++ MYSQL Queries.sql
@@ -19,23 +19,25 @@ select min(quantity) from wallmart;
 
 -- Bussiness Problems
 
--- Q-1. Find different payment methods, number of transactions, and quantity sold by payment method
-       select  payment_method,count(*) as no_of__payment,sum(quantity) as no_of_quantity_sold 
-       from wallmart
-       group by payment_method;
+-- Q-1. Find different payment methods, number of transactions, and quantity sold by payment method 
+        select  payment_method,count(*) as no_of__payment,sum(quantity) as no_of_quantity_sold 
+        from wallmart
+        group by payment_method;
 
--- 	Q-2. Identify the highest-rated category in each branch, Display the branch, category, and avg rating
-         select t.branch,t.category,t.avg_rating from (select branch,category,round(avg(rating),2) as avg_rating,rank() over(partition by branch order by avg(rating) desc) as Ranks
-         from wallmart 
-		 group by branch,category
-		 order by branch,avg_rating) as t
-		 where t.ranks=1;
+-- Q-2. Identify the highest-rated category in each branch, Display the branch, category, and avg rating
+        select t.branch, t.category, t.avg_rating 
+		from  (select branch, category, round(avg(rating),2) as avg_rating,rank() over(partition by branch order by avg(rating) desc) as Ranks
+               from wallmart 
+		       group by branch,category
+		       order by branch,avg_rating) as t
+		where t.ranks=1;
          
 -- Q3: Identify the busiest day for each branch based on the number of transactions
-       select t.branch,t.day_name,t.no_of_transaction from (select branch,dayname(date) as day_name,count(*) as no_of_transaction,rank() over(partition by branch order by count(*) desc) as ranks
-       from wallmart
-       group by branch,day_name
-       order by branch) as t
+       select t.branch,t.day_name,t.no_of_transaction
+	   from (select branch,dayname(date) as day_name,count(*) as no_of_transaction,rank() over(partition by branch order by count(*) desc) as ranks
+             from wallmart
+             group by branch,day_name
+             order by branch) as t
        where t.ranks=1;
        
 -- Q4: Calculate the total quantity of items sold per payment method
@@ -82,13 +84,15 @@ select min(quantity) from wallmart;
        group by branch
        order by branch),
        
-	   revenue_2023 as ( select branch,round(sum(total),2) as revenue_2023
+	   revenue_2023 as (select branch,round(sum(total),2) as revenue_2023
        from wallmart
        where year(date)=2023
        group by branch
        order by branch)
        
-       select r1.branch,r1.revenue_2022 as prev_revenue,r2.revenue_2023 as current_revenue,round((r1.revenue_2022-r2.revenue_2023)*100.0/r1.revenue_2022,2) as revenue_decrease_ratio
+       select r1.branch, r1.revenue_2022 as prev_revenue,
+	   r2.revenue_2023 as current_revenue,
+	   round((r1.revenue_2022-r2.revenue_2023)*100.0/r1.revenue_2022,2) as revenue_decrease_ratio
        from revenue_2022 as r1
        join
        revenue_2023 as r2
